cee2fbe2854d7a55fc4c838475f251053e0e8311
codermdshakil <ahmedshakil0512@gmail.com>
2024-06-19 18:02:29 +0600

    Learn: Time complexity - O(1)

---
+++ space_complexity.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if (n % 2 == 0)
+    {
+        cout << "Even " << n;
+    }
+    else
+    {
+        cout << "Odd " << n;
+    }
+    return 0;
+}
+
+// time complexity - O(1)
+// space complexity - O(1) - একটাই operator হইতাসে
