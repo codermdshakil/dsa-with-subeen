103ff87091face7afab9fd0803f29038ea6a6ab8
codermdshakil <ahmedshakil0512@gmail.com>
2024-06-19 18:16:53 +0600

    Learn : Space Complexity O(n^2)

---
+++ space_complexity_3.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // two matrix sum 
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //        s[i][j] = a[i][j] + b[i][j];
+    //     }
+        
+    // }
+
+ 
+    return 0;
+}
+
+// space complexity is - O(n^2)
