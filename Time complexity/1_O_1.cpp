72b61316dd097cd4e76d19e7f682d0588952397b
codermdshakil <ahmedshakil0512@gmail.com>
2024-06-19 16:07:54 +0600

    Learn: O(1) time complexity

---
+++ Time complexity/1_O_1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n1, n2, result;
+    n1 = 10;
+    n2 = 20;
+    result = n1 + n2; // O(1)
+    cout << result << endl;
+    return 0;
+}
+
+/**
+ * O(1) Time Complexity
+ * Input এর মানের সাথে Operation এর সংখ্যার নীরবর করে না তাই O(1) - Big O notation.
+*/
